3cd804f2c61640a3da89929b3fe55bbe5d36b7f3
Jennifer Zhang <jenniferjuliazhang@gmail.com>
2016-07-09 12:56:34 -0700

    Uploading and getting videos

--- app/index.py
+++ app/index.py
@@ -1,28 +1,49 @@
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/videos'
-ALLOWED_EXTENSIONS = set(['.mp4', 'mov'])
+UPLOAD_FOLDER = 'videos'
+ALLOWED_EXTENSIONS = set(['mp4', 'mov'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
 	return 'Hello, World!'
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['POST', 'GET'])
 def upload():
 	if request.method == 'POST':
-		# Grab video from request data
-		# Save video locally
-		request.data
-		return 'upload'
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		video = request.files['file']
+		if video.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if video and allowed_file(video.filename):
+			filename = secure_filename(video.filename)
+			video.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return url_for('uploaded_file', filename=filename)
+	return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+	'''
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
-@app.route('/video/<int:video_id>', methods=['GET'])
-def getVideo(video_id):
-	return str(video_id)
+def allowed_file(filename):
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.route('/create', methods=['POST'])
@@ -32,4 +53,4 @@ def createVideo():
 
 
 if (__name__ == '__main__'):
-	app.run(debug=True)
+	app.run(debug=True, threaded=True)
