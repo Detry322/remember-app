7559acfbe6bf331d8289db82df6f42fb3ca55546
Jack Serrino <Jserrino@gmail.com>
2016-07-09 21:51:48 -0700

    Integration part 2

--- app/index.py
+++ app/index.py
@@ -5,46 +5,38 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = 'videos'
 ALLOWED_EXTENSIONS = set(['mp4', 'mov'])
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
-	return 'Hello, World!'
+	return send_from_directory('views', 'index.html')
 
-@app.route('/upload', methods=['POST', 'GET'])
+@app.route('/app')
+def app_dir():
+	return send_from_directory('views', 'app.html')
+
+@app.route('/upload', methods=['POST'])
 def upload():
-	if request.method == 'POST':
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		print 'REQUEST', request
-		video = request.files['file']
-		if video.filename == '':
-			flash('No selected file')
-			return redirect(request.url)
-		if video and allowed_file(video.filename):
-			filename = secure_filename(video.filename)
-			video.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return url_for('uploaded_file', filename=filename)
-	return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-	'''
+	if 'file' not in request.files:
+		flash('No file part')
+		return 'No file part', 400
+	print 'REQUEST', request
+	video = request.files['file']
+	if video.filename == '':
+		flash('No selected file')
+		return 'No selected file', 400
+	if video and allowed_file(video.filename):
+		filename = secure_filename(video.filename)
+		video.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return url_for('uploaded_file', filename=filename), 200
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 def allowed_file(filename):
-	return '.' in filename and \
-		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 '''
 Params:
